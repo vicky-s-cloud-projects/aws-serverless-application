a8ae497716c80d9af20ce84b2636ae7f187c68f8
vivekchalla246 <vivekchalla246@gmail.com>
2026-02-01 22:00:23 +0000

    Use Terraform output for CloudFront invalidation in CI

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,8 @@ output "frontend_url" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.frontend.bucket
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend.id
+}
+
